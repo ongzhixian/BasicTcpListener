fdaa51b4ee0715d1d85385d38633f0a5d4cd2253
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-05-13 11:36:09 +0800

    init commit

---
+++ BasicTcpListener.ConsoleApp/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+TcpListener? server = null;
+
+try
+{
+    ReadIPAddressPortFromConfig(out IPAddress ipAddress, out int port);
+
+    server = new TcpListener(ipAddress, port);
+    
+    Console.WriteLine($"Listening for connections on {ipAddress}:{port}");
+
+    server.Start();
+
+    while (true)
+    {
+        Console.Write("Waiting for a connection... ");
+
+        // Perform a blocking call to accept requests.
+        // You could also use server.AcceptSocket() here.
+        using (TcpClient client = server.AcceptTcpClient())
+        using (NetworkStream stream = client.GetStream())
+        {
+            Console.WriteLine("Connected!");
+
+            byte[] bytes = new byte[256];
+
+            string? data = null;
+
+            int i;
+
+            // Loop to receive all the data sent by the client.
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                // Translate data bytes to a ASCII string.
+                data = Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+
+                // Process the data sent by the client.
+                data = data.ToUpper();
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                // Send back a response.
+                stream.Write(msg, 0, msg.Length);
+                Console.WriteLine("Sent: {0}", data);
+            }
+        }
+    }
+}
+catch (SocketException e)
+{
+    Console.WriteLine("SocketException: {0}", e);
+}
+finally
+{
+    if (server != null)
+    {
+        server.Stop();
+        Console.WriteLine("Server stopped.");
+    }
+}
+
+void ReadIPAddressPortFromConfig(out IPAddress ipAddress, out int port)
+{
+    const string HOST_CONFIGURATION_KEY = "TcpListener:Host";
+    const string PORT_CONFIGURATION_KEY = "TcpListener:Port";
+
+    ipAddress = IPAddress.Loopback;
+    port = 3092;
+
+    IConfiguration configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+
+    if (IPAddress.TryParse(configuration[HOST_CONFIGURATION_KEY], out IPAddress? parsedIPAddress))
+    {
+        ipAddress = parsedIPAddress;
+    }
+
+    if (int.TryParse(configuration[PORT_CONFIGURATION_KEY], out int parsedPort))
+    {
+        port = parsedPort;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # BasicTcpListener
-A basic .NET Core TCO listener console application use for simple deployments in Kubernetes. 
+
+A basic .NET Core TCP listener console application use for simple deployments in Kubernetes. 
+
+
+## dotnet CLI
+
+dotnet CLI used to create this project:
+
+```ps1: In C:\src\github.com\ongzhixian\BasicTcpListener
+dotnet new sln -n BasicTcpListener
+dotnet new console -n BasicTcpListener.ConsoleApp
+dotnet sln .\BasicTcpListener.sln add .\BasicTcpListener.ConsoleApp\
+
+dotnet add .\BasicTcpListener.ConsoleApp\ package Microsoft.Extensions.Configuration
+dotnet add .\BasicTcpListener.ConsoleApp\ package Microsoft.Extensions.Configuration.Json
+
+```
+
+Other packages that we may want to include to expand on configuration options:
+Microsoft.Extensions.Configuration.CommandLine
+Microsoft.Extensions.Configuration.Binder
+Microsoft.Extensions.Configuration.EnvironmentVariables 
